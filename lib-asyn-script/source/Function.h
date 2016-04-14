07640839140cfa44f28f50f9e0a8b3d9333b2ac9
ThinEureka <04nycs@gmail.com>
2016-04-14 09:57:49 +0800

    Optimize ASYS_VAR definition
    Optimize ASYS_VAR definition (thanks to 逍遥)

--- lib-asyn-script/source/Function.h
+++ lib-asyn-script/source/Function.h
@@ -25,8 +25,8 @@
 #define _ ;
 #endif
 
-#define ASYS_VAR(name) const std::string name(asys::toAsysVarName(#name))
-#define ASYS_PARAM(name) const std::string& name = asys::toAsysVarName(#name)
+#define ASYS_VAR(name) const std::string name("$"#name)
+#define ASYS_PARAM(name) const std::string& name = "$"#name
 #define D_ASYS_PARAM(name) const std::string& name
 
 namespace asys
